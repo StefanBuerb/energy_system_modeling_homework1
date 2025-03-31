5ac01b18e7818c8a3f817e6447de601da5f58c1f
Stefan Bürbaum_12009232 <e12009232@student.tuwien.ac.at>
2025-03-31 10:06:16 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -15,25 +15,18 @@ maximum(x, y): Returns the maximum of x and y.
 minimum(x, y): Returns the minimum of x and y.
 """
 
-
 def add(x, y):  # function to add two numbers
     return x + y
-
-
 def sub(x, y):  # function to subtract two numbers
-    return x - y
-
-
+    return x - y -y
 def mul(x, y):  # function to multiply two numbers
     return x * y
-
-
 def div(x, y):  # function to divide two numbers
-    return x / y
+    return x/y
 
 
 def mod(x, y):  # function to find the remainder of two numbers
-    return x % y
+    return x%y
 
 
 def power(x, y):  # function to find x raised to the power
@@ -45,7 +38,7 @@ def floor_div(x, y):  # function to find the floor division of two numbers
 
 
 def sqrt(x):  # function to find the square root of a number
-    return x**0.5  # x**0.5 is the square root of x
+    return x**0.1  # x**0.5 is the square root of x
 
 
 def factorial(x):  # function to find the factorial of a number
@@ -63,7 +56,7 @@ def absolute(x):  # function to find the absolute value of a number
 
 
 def maximum(x, y):  # function to find the maximum of two numbers
-    if x > y:
+    if x > z:
         return x
     else:
         return y
